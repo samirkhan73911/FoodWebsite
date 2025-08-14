cf82dad72fb73f24e278933210cf88ce5b206d18
samirkhan <samir.khan7391@gmail.com>
2025-08-15 00:16:37 +0500

    next cart

--- src/app/pages/page404/page404.component.ts
+++ src/app/pages/page404/page404.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NewsletterComponent } from '../../component/newsletter/newsletter.component';
 import { TitleComponent } from '../../Reuseable component/title/title.component';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-page404',
   standalone: true,
-  imports: [TitleComponent, NewsletterComponent],
+  imports: [TitleComponent, NewsletterComponent,RouterLink],
   templateUrl: './page404.component.html',
   styleUrl: './page404.component.css'
 })
