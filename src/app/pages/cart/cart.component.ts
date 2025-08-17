3e8c515705e1dc3d9c6d69498f2083b7e7a74d5e
samirkhan <samir.khan7391@gmail.com>
2025-08-18 00:14:21 +0500

    cart design

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -2,15 +2,28 @@ import { Component } from '@angular/core';
 import { NewsletterComponent } from '../../component/newsletter/newsletter.component';
 import { TitleComponent } from '../../Reuseable component/title/title.component';
 import { CardComponent } from '../../Reuseable component/card/card.component';
+import { RatingComponent, RatingModule } from 'ngx-bootstrap/rating';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [NewsletterComponent, TitleComponent, CardComponent],
+  imports: [NewsletterComponent, TitleComponent, CardComponent,RatingComponent,FormsModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
+  max = 5;
+  rate = 2;
+ 
+ 
+  confirmSelection(event: KeyboardEvent) {
+    if (event.keyCode === 13 || event.key === 'Enter') {
+     
+    }
+  }
+ 
 
 }
